e29e25150cc96ba27c1be9d1fa10b4ea74bbf283
virendramali <virendravmali77@gmail.com>
2026-02-04 10:55:34 +0530

    Modified assignment_1.py and removed assignment_2.py

--- python/assignment_1.py
+++ python/assignment_1.py
@@ -16,3 +16,17 @@ print("Addition: ", num_1 + num_2)
 print("Subtraction: ", num_1 - num_2)
 print("Multiplication: ", num_1 * num_2)
 print("Division: ", num_1 / num_2)
+
+###################################################
+
+"""
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f"Hello, {first_name + ' ' + last_name}! Welcome to the Python program.")
