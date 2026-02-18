e12a5593b0491004389d5d255a1d86efdca6ca72
virendramali <virendravmali77@gmail.com>
2026-02-18 21:39:58 +0530

    Added Python assignment 2

--- python/assignment_2.py
+++ python/assignment_2.py
@@ -1,11 +1,27 @@
-"""
-Task 2: Create a Personalized Greeting
-Problem Statement: Write a Python program that:
-1.  Takes a user's first name and last name as input.
-2.  Concatenates the first name and last name into a full name.
-3.  Prints a personalized greeting message using the full name.
+"""Task 1: Check if a Number is Even or Odd
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
 """
 
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-print(f"Hello, {first_name + ' ' + last_name}! Welcome to the Python program.")
+num = int(input("Enter a number: "))
+(
+    print(f"{num} is an even number.")
+    if num % 2 == 0
+    else print(f"{num} is an odd number.")
+)
+
+###################################################
+
+"""
+Task 2: Sum of Integers from 1 to 50 Using a Loop
+Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+"""
+count = 0
+for i in range(1, 51):
+    count += i
+print(f"The sum of numbers from 1 to 50 is: {count}")
